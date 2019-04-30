d44991823f2af89fc7c6bc8f55145384716c2d4d
丁巍 <dingwei2014@xiaochuankeji.cn>
2019-04-30 11:03:19 +0800

    ADD : ① 添加pod 所需 dataSource

---
+++ Sources/DWAdapter/RuntimeMethod/DWIocComponent.h
@@ -0,0 +1,40 @@
+//
+//  DWIocComponent.h
+//  DWVideoPlay
+//
+//  Created by 丁巍 on 2019/3/11.
+//  Copyright © 2019 丁巍. All rights reserved.
+//
+
+//  runtime 调用方法组件
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define IsEmpty(str)            (str == nil || ![str respondsToSelector:@selector(isEqualToString:)] || [str isEqualToString:@""])
+#define IsNull(obj)             (obj == nil || [obj isEqual:[NSNull null]])
+
+typedef void(^methodResultBlock)(id __nullable returnObj);
+
+@interface DWIocComponent : NSObject
+
+
+/** 获取对象 */
++(id)getInstanceByClassName:(NSString *)className;
+
+
+/**
+ 方法调用
+ methodClass  : 实例对象
+ functionName : 方法名称
+ objects      : 方法参数(顺序执行)
+ 
+ return       : 方法调用返回值
+ */
++(id)methodCallWithMethodClass:(id)methodClass functionName:(NSString *)functionName objects:(NSArray *__nullable)objects;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
